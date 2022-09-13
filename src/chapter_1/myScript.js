482bc5554a75146640efd04942d6c41a7d38c435
Meadows Ellis <meadowse@si-o1.kzn.21-school.ru>
2022-09-13 13:47:21 +0300

    finish

---
+++ src/chapter_1/myScript.js
@@ -0,0 +1,10 @@
+let display = document.getElementById("display")
+function clear1() {
+    display.value = ""
+}
+function calc() {
+    display.value = eval(display.value)
+}
+function press(x) {
+    display.value += x;
+}
